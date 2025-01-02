ecedab27c4586edd52d24a4d4710fe9e6d692fe9
Timo Kirkkala <timo.kirkkala@gmail.com>
2025-01-02 23:31:48 +0200

    Restructure to component based UI with AI

---
+++ components/Form/SelectField/SelectField.tsx
@@ -0,0 +1,46 @@
+import { IconType } from 'react-icons'
+import styles from './SelectField.module.scss'
+
+interface SelectFieldProps {
+  id: string
+  label: string
+  description: string
+  Icon: IconType
+  options: Array<{
+    value: string
+    label: string
+  }>
+  defaultValue?: string
+}
+
+export default function SelectField({
+  id,
+  label,
+  description,
+  Icon,
+  options,
+  defaultValue
+}: SelectFieldProps) {
+  return (
+    <div className={styles.formGroup}>
+      <label htmlFor={id}>
+        <Icon /> {label}
+      </label>
+      <p className={styles.fieldDescription}>
+        {description}
+      </p>
+      <select
+        id={id}
+        name={id}
+        defaultValue={defaultValue}
+      >
+        <option value="">- Valitse -</option>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
